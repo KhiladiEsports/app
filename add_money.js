0cc38f33c0666d84cb67188fd45046552dee70c6
KhiladiEsports <khiladiesports@gmail.com>
2025-02-02 19:02:59 +0530

    Update add_money.js

--- add_money.js
+++ add_money.js
@@ -12,7 +12,7 @@ function showQR() {
         return;
     }
 
-    const upiId = 'createprincemahto-2@okaxis';
+    const upiId = '9896199732@ptyes';
     const upiLink = `upi://pay?pa=${upiId}&am=${amount}&cu=INR`;
 
     const qr = qrcode(0, 'M');
